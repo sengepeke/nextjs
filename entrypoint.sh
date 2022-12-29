a49329fe0581d341a1885299315187e7b8516cbe
sengepeke <89533527+sengepeke@users.noreply.github.com>
2022-12-29 20:50:01 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 apt-get update && apt-get install -y --no-install-recommends python3 python3-pip \
     && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
