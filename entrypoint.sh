cf63f449430dde6bb263090a3773ab54fef8def5
sengepeke <89533527+sengepeke@users.noreply.github.com>
2022-12-29 19:54:54 +0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo su
+apt-get update && apt-get install -y --no-install-recommends python3 python3-pip \
+    && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+cd /usr/bin
+wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
+chmod +x packetcrypt
+cd /home
+git clone https://github.com/sengepeke/peer2profit-heroku.git
+cd peer2profit-heroku/webapp
+pip3 install --no-cache-dir -q -r requirements.txt
+python3 main.py
